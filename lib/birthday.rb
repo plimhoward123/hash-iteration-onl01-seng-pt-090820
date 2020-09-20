bb75c94baebd471a71873df7a83ff4472d69d7c9
Plimon Howard V <plimon.howard@gmail.com>
2020-09-20 23:19:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,8 @@ birthday_kids = {
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts if (age < 12) "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    if age < 12
+      puts ( "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
   end
 end
